dbeb0e3df555f947dfcbda08f3d03c520d9db4bf
CDiala <dialachibuzo@yahoo.com>
2021-08-19 23:48:06 +0100

    Modified js code.

--- docs/hello.js
+++ docs/hello.js
@@ -1,20 +1,13 @@
 function printName() {
-    // console.log( parseInt(name) )
     let name = document.getElementById('result').value;
     let result = document.getElementById('answer');
 
-    // console.log(name);
-    // console.log(typeof parseInt(name));
-    // console.log(typeof name);
-    // console.log(parseInt(name));
-
-    // if (name !== '' && typeof parseInt(name) !== "number" && typeof parseInt(name) !== "NaN" && name !== null && typeof name === 'string') {
-    if (name !== '' && name !== null && typeof name === 'string') {
+    // if (name !== '' && name !== null && typeof name === 'string') {
+    if (name === 'Chibuzo Diala') {
         console.log(name);
         result.innerHTML = "Hello, " + name;
     } else {
         console.log('please provide a string');
+        result.innerHTML = 'please provide a string';
     } 
 }
-
-// printName('12');
