aac73e6d99017338de1e4575ff9043646b50f8b1
CDiala <dialachibuzo@yahoo.com>
2021-08-19 23:49:37 +0100

    Modified JS code

--- docs/hello.js
+++ docs/hello.js
@@ -5,7 +5,7 @@ function printName() {
     // if (name !== '' && name !== null && typeof name === 'string') {
     if (name === 'Chibuzo Diala') {
         console.log(name);
-        result.innerHTML = "Hello, " + name;
+        result.innerHTML = name;
     } else {
         console.log('please provide a string');
         result.innerHTML = 'please provide a string';
